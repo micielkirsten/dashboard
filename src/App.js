3775fdc7771a1d30451109614fcb76fff6f5835d
Kirsten P <micielkirsten@gmail.com>
2022-07-01 23:35:36 -0400

    starting on orders page

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
                         <Route path = "/ecommerce" element= { < Ecommerce /> } />
 
                         {/* Pages */}
-                        <Route path = "/oders" element= { < Orders />} />                        
+                        <Route path = "/orders" element= { < Orders />} />                        
                         <Route path = "/employees" element= { < Employees />} />
                         <Route path = "/customers" element= { < Customers />} /> 
 
